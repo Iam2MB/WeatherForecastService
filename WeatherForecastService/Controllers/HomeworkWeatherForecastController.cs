1120c40915db4f4a7c9523b1d0acc5212ebba0f5
User <User@DESKTOP-UI7FTIO>
2022-09-27 18:44:10 +0300

    Add project files.

---
+++ WeatherForecastService/Controllers/HomeworkWeatherForecastController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherForecastService.Models;
+
+namespace WeatherForecastService.Controllers
+{
+    [Route("api/weather")]
+    [ApiController]
+    public class HomeworkWeatherForecastController : ControllerBase
+    {
+        private readonly WeatherForcastHolder _holder;
+
+        public HomeworkWeatherForecastController(WeatherForcastHolder holder)
+        {
+            _holder = holder;
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add([FromQuery] DateTime date, [FromQuery] int temperatureC)
+        {
+            _holder.Add(date, temperatureC);
+            return Ok();
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromQuery] DateTime date, [FromQuery] int temperatureC)
+        {
+            _holder.Add(date, temperatureC);
+            return Ok();
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] DateTime date)
+        {
+            _holder.Delete(date);
+            return Ok();
+        }
+
+        [HttpGet("get")]
+        public IActionResult Get([FromQuery] DateTime dateFrom, [FromQuery] DateTime dateTo)
+        {
+            return Ok(_holder.Get(dateFrom, dateTo));
+        }
+    }
+}
---
+++ WeatherForecastService/Controllers/Test2Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherForecastService.Controllers
+{
+    [Route("api/test2")]
+    [ApiController]
+    public class Test2Controller : ControllerBase
+    {
+
+    }
+}
